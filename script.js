0302da7c60579ea5794d374fef0fc9bd013c73a9
psalmalika <99105416+psalmalika@users.noreply.github.com>
2022-02-25 12:39:34 +0700

    Update script.js

--- script.js
+++ script.js
@@ -75,10 +75,6 @@ const displayAmiibo = (amiibos) => {
         character = amiibo.character
         series = amiibo.amiiboSeries;
         game = amiibo.gameSeries;
-        type = amiibo.type;
-        head = amiibo.head;
-        tail = amiibo.tail;
-        number = amiibo.head + amiibo.tail;
 
         const amiiboCard = `
             <div class="amiibo-card">
@@ -91,33 +87,77 @@ const displayAmiibo = (amiibos) => {
                     <p class="amiibo-character">Character: ${character}</p>
                     <p class="amiibo-series">Amiibo Series: ${series}</p>
                     <p class="amiibo-game">Game Series: ${game}</p>
-                    <p class="amiibo-type">Type: ${type}</p>
-                    <p class="amiibo-number">Number: ${number};</p>
-                    <button class="button button-details" id="button-details-${i}" value=${number} onClick="openModal()"><p>Details</p></button>
+                    <button class="button button-details" onClick="openModal(${i})")><p>Details</p></button>
                 </div>
             </div>
         `
-        
+
         listOfAmiibo += amiiboCard;
+        /* let buttonDetails = document.getElementById(`button-details-${i}`);
+
+        buttonDetails.addEventListener("click", openModal(i)); */
     };
 
     amiiboDiv.innerHTML = listOfAmiibo;
+
 };
 
-const openModal = async() => {
+const amiiboPage = document.querySelector('.amiibo-page');
+
+const openModal = async function (e) {
+    //const res = await findAmiibo(keyword);
+    //console.log('filteredAmiibo di openModal:')
+    //console.log(filteredAmiibo);
+    image = filteredAmiibo[e].image;
+    name = filteredAmiibo[e].name;
+    character = filteredAmiibo[e].character;
+    series = filteredAmiibo[e].amiiboSeries;
+    game = filteredAmiibo[e].gameSeries;
+    type = filteredAmiibo[e].type;
+    head = filteredAmiibo[e].head;
+    tail = filteredAmiibo[e].tail;
+    identifier = filteredAmiibo[e].head + filteredAmiibo[e].tail;
+    releaseAU = filteredAmiibo[e].release.au;
+    releaseEU = filteredAmiibo[e].release.eu;
+    releaseJP = filteredAmiibo[e].release.jp;
+    releaseNA = filteredAmiibo[e].release.na;
+    console.log(releaseAU);
+
+    document.getElementById('amiibo-page-image').src = `${image}`;
+    document.getElementById('amiibo-page-name').innerHTML = name;
+    document.getElementById('amiibo-page-character').innerHTML = `<span class="page-title">Karakter:</span> ${character}`;
+    document.getElementById('amiibo-page-amiibo-series').innerHTML = `<span class="page-title">Amiibo Series:</span> ${series}`;
+    document.getElementById('amiibo-page-game-series').innerHTML = `<span class="page-title">Game Series:</span> ${game}`;
+    document.getElementById('amiibo-page-type').innerHTML = `<span class="page-title">Type:</span> ${type}`;
+    document.getElementById('amiibo-page-release-au').innerHTML = `${releaseAU} (Australia)`;
+    document.getElementById('amiibo-page-release-eu').innerHTML = `${releaseEU} (Europe)`;
+    document.getElementById('amiibo-page-release-jp').innerHTML = `${releaseJP} (Japan)`;
+    document.getElementById('amiibo-page-release-na').innerHTML = `${releaseNA} (North America)`;
+
+    // console.log(e.name);
+    console.log(amiiboPage);
+    // amiiboPage.classList.toggle('show-amiibo-page');
+    amiiboPage.style.display = 'block';
+}
+
+const closeModal = () => {
+    amiiboPage.style.display = 'none';
+}
+
+/* const openModal = async(x) => {
     console.log('Tombol Details diklik');
     await findAmiibo(filteredAmiibo);
     console.log('filteredAmiibo di openModal:')
     console.log(filteredAmiibo);
 
-    /* value1 = document.getElementById("button-details-0").value;
-    console.log(value1); */
+    value1 = document.getElementById("button-details-0").value;
+    console.log(value1);
     
-    /* Nemu i dari value button Details (seharusnya berbeda setiap card) */
+    // Nemu i dari value button Details (seharusnya berbeda setiap card)
     let x = document.getElementById(`button-details-${i}`).value;
     console.log(x);
     
     name = filteredAmiibo[x].name;
     console.log('Akan buka pop up page');
     console.log(name);
-}
+} */
